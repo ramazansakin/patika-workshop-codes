af88a1dac256c8cc7a991d35cefb9f2e4fce163c
ramazansakin <ramazansakin63@gmail.com>
2024-01-10 06:35:02 +0300

    week2 sample codes up

--- week2/MyArrayList.java
+++ week2/MyArrayList.java
@@ -1,6 +1,7 @@
 package week2;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.ListIterator;
 
 public class MyArrayList {
     private static final int INITIAL_CAPACITY = 10;
@@ -21,7 +22,7 @@ public class MyArrayList {
 
     @SuppressWarnings("unchecked")
     public Integer get(int index) {
-        if (index < 0 || index >= size) {
+        if (index < 0 || index >= INITIAL_CAPACITY) {
             throw new IndexOutOfBoundsException("Index out of bounds: " + index);
         }
         return array[index];
@@ -36,11 +37,10 @@ public class MyArrayList {
         array = Arrays.copyOf(array, newCapacity);
     }
 
-    public int indexOf(Object o) {
-        ListIterator<Integer> it = (ListIterator<Integer>) Arrays.stream(array).toList().iterator();
-        while (it.hasNext()){
-            if(it.next().equals(o))
-                return it.nextIndex();
+    public int indexOf(Integer number) {
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == number)
+                return i;
         }
         return -1;
     }
@@ -49,10 +49,38 @@ public class MyArrayList {
     // How to use MyLinkedList
     public static void main(String[] args) {
 
-        System.out.println("################  APPLICATION START ");
+        System.out.println("################  APPLICATION STARTED ################");
         MyArrayList myArrayList = new MyArrayList();
 
+        System.out.println("Size : " + myArrayList.size());
 
+        myArrayList.add(5);
+        myArrayList.add(15);
+        myArrayList.add(1);
+        myArrayList.add(40);
+
+        System.out.println("Size : " + myArrayList.size());
+
+        System.out.println("Index 3 value : " + myArrayList.get(3));
+        System.out.println("Index 5 value : " + myArrayList.get(5));
+
+        // Exception Handling
+        try {
+            Integer result = myArrayList.get(15);
+            System.out.println("15. Index value : " + result);
+        } catch (Exception exp) {
+            System.err.println("A problem occurred : " + exp.getMessage());
+        }
+
+        try {
+            int index = myArrayList.indexOf(40);
+            System.out.println("Index of 40 : " + index);
+        } catch (Exception exp) {
+            System.err.println("A problem occurred : " + exp.getMessage());
+        }
+
+        System.out.println("Index of 40 : " + myArrayList.indexOf(20));
+        System.out.println("################  APPLICATION STOPPED ################");
 
     }
 
